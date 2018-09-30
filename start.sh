fbcfe0cf0c00a2b8e8a4f4bd3109bf699b7799c5
marcin <marcin.kasinski@gmail.com>
2018-09-30 10:06:01 +0200

    .

--- start.sh
+++ start.sh
@@ -18,8 +18,7 @@ HOSTNAME=`hostname -f`
 
 
 #if [ "$AUTH_TYPE" == "SASL_PLAINTEXT" ]; then 
-if [[ $KAFKA_PARAM_advertised_listeners =~ .*SASL_PLAINTEXT.* ]]
-then
+if [[ $KAFKA_PARAM_advertised_listeners =~ .*SASL_PLAINTEXT.* ]]; then 
   echo "THERE IS SASL"
   KAFKA_OPTS="$KAFKA_OPTS -Djava.security.auth.login.config=$JAAS_FILE_LOCATION"
 fi
