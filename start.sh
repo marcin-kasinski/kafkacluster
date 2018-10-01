1fcb1f4f5479c6f47a2a81ab456530d1078ca31c
marcin <marcin.kasinski@gmail.com>
2018-10-01 11:12:39 +0200

    .

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ processBROKER_NODES
 
 process_param_config
 
-echo replacing {HOSTNAME_FQDN} to $HOSTNAME_FQDN
+echo replacing {HOSTNAME_FQDN} with $HOSTNAME_FQDN
 cp $JAAS_FILE_LOCATION_RO $JAAS_FILE_LOCATION
 sed -i -e 's/{HOSTNAME_FQDN}/'"$HOSTNAME_FQDN"'/g' $JAAS_FILE_LOCATION
 
