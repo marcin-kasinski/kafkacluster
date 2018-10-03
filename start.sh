eca85169c4039c3ee70f63bc8e1e3fa993c78d57
marcin <marcin.kasinski@gmail.com>
2018-10-03 19:32:47 +0200

    .

--- start.sh
+++ start.sh
@@ -9,17 +9,25 @@ echo "" >$CONFIG
 HOSTNAME_FQDN=`hostname -f`
 
 #if [ "$AUTH_TYPE" == "SASL_PLAINTEXT" ]; then 
-EXTRA_KAFKA_OPTS="$KAFKA_OPTS $EXTRA_JAVA_ARGS -Djava.security.auth.login.config=$JAAS_FILE_LOCATION"
+#EXTRA_KAFKA_OPTS="$KAFKA_OPTS $EXTRA_JAVA_ARGS -Djava.security.auth.login.config=$JAAS_FILE_LOCATION"
 #fi
+
+if [ -z "$EXTRA_KAFKA_OPTS" ]; then 
+  #there is EXTRA_KAFKA_OPTS
+  EXTRA_KAFKA_OPTS="$KAFKA_OPTS $EXTRA_JAVA_ARGS
+fi
 
 processBROKER_NODES
 
 process_param_config
 
-echo replacing {HOSTNAME_FQDN} with $HOSTNAME_FQDN
-cp $JAAS_FILE_LOCATION_RO $JAAS_FILE_LOCATION
-sed -i -e 's/{HOSTNAME_FQDN}/'"$HOSTNAME_FQDN"'/g' $JAAS_FILE_LOCATION
-cat $JAAS_FILE_LOCATION
+if [ -z "$JAAS_FILE_LOCATION" ]; then 
+  #there is JAAS_FILE_LOCATION
+  echo replacing {HOSTNAME_FQDN} with $HOSTNAME_FQDN
+  cp $JAAS_FILE_LOCATION_RO $JAAS_FILE_LOCATION
+  sed -i -e 's/{HOSTNAME_FQDN}/'"$HOSTNAME_FQDN"'/g' $JAAS_FILE_LOCATION
+  cat $JAAS_FILE_LOCATION
+fi
 
 echo "Configuration"
 cat $CONFIG
